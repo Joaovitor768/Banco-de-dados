c47a158a63c3abaaab1c0770b4a1a47b02cc716e
Alexandre12424 <alexandredelfino.ad@gmail.com>
2026-05-13 22:45:55 -0300

    feito

--- Conexão.php
+++ Conexão.php
@@ -1,11 +1,16 @@
 <?php
 
 $hostname = "localhost";
-$bancodedados = "banco de dados";
+$bancodedados = "lord_camisetas";
 $usuario = "usuario";
-$senha = "senha";
+$senha = "";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 if ($mysqli->connect_errno) {
-    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "Falha ao conectar:(" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
 }
+else {
+    echo "Conectado com sucesso!";
+}
+
+?>
